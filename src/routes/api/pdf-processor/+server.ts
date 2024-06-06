29a6fa3b21e25fce672c3023dd2f0dc9fca1d095
Fast DMS Corey <corey.damocles@fastdms.com>
2024-06-05 18:37:35 -0700

    Update GPT model and prompt

--- src/routes/api/pdf-processor/+server.ts
+++ src/routes/api/pdf-processor/+server.ts
@@ -35,12 +35,12 @@ export const POST: RequestHandler = async ({ request }) => {
 		}
 
 		const openaiResponse = await openai.chat.completions.create({
-			model: 'gpt-3.5-turbo-1106',
+			model: 'gpt-4-turbo-2024-04-09',
 			messages: [
 				{
 					role: 'system',
 					content:
-						'You are a manufacturing engineer assistant. Extract and respond with the Part Number, Description, Revision, and a list of succinct recommended operations (like milling, galvanizing, laser cutting, bead blasting, etc.) based on the materials and details provided. Return the data in a JSON structure like this: {"part_number": "<Part/Drawing Number>", "description": "<Description/Title>", "revision": "<Revision Number/Letter>", "operations": ["<Operation1>", "<Operation2>", ...]}'
+						'You are a manufacturing engineer assistant. Extract and respond with the Part Number, Description, Revision, and a list of succinct recommended operations (like milling, galvanizing, laser cutting, bead blasting, etc.) based on the materials and details provided. Return the data in a JSON structure like this: {"part_number": "<Part/Drawing Number>", "description": "<Description/Title>", "revision": "<Revision Number/Letter>", "operations": ["<Operation1>", "<Operation2>", ...]} Don\'t return the result encapsulated in ``` Part Number is an alphanumeric string that can be found in the lower right part of the PDF. It is displayed prominently, usually in a box. It is generally near words like "Part Number", "PN", "Document Number", "Part #", or something similar. It is usually the largest font size in the area. When there are multiple sheets in the PDF, the Part Number is usually displayed in the same location on every sheet. Description is an alphanumeric string that can be found in the lower right part of the PDF. It is displayed slightly less prominently than the Part Number, usually in a box. It is generally near words like "Description", "Title", "Document Name", "Part Name", "Name", or something similar. It is usually an average font size. It is usually longer than the Part Number and may have multiple words. When there are multiple sheets in the PDF, the Description is usually displayed in the same location on every sheet. No prefix is needed for this, such as "Assembly Drawing:". Revision is an alphanumeric string that can be found in the bottom right part of the drawing, near the part number or description fields. It is typically in a box that only contains the revision under or near words like "Rev". It is usually 1-3 characters and is different from the "Size" field which is also in the lower right and labeled SIZE. The revision number is usually displayed in a slightly less prominent font than the Part Number. Ensure you do not confuse the revision number with the size field or other labels.'
 				},
 				{
 					role: 'user',
@@ -49,6 +49,7 @@ export const POST: RequestHandler = async ({ request }) => {
 			]
 		})
 
+		console.log('pdfText: ', pdfText)
 		console.log('openaiResponse:', openaiResponse)
 		let responseText = openaiResponse.choices[0].message.content
 		if (responseText) {
