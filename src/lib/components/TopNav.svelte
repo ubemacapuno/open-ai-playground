600a4f1931f7324a684e46b4f9e6983a368a455d
Fast DMS Corey <corey.damocles@fastdms.com>
2024-07-02 09:23:41 -0700

    Pass currentUser to TopNav

--- src/lib/components/TopNav.svelte
+++ src/lib/components/TopNav.svelte
@@ -2,6 +2,10 @@
 	import { page } from '$app/stores'
 	import LightSwitch from './LightSwitch.svelte'
 	import { ModeWatcher } from 'mode-watcher'
+	import { applyAction, enhance } from '$app/forms'
+	import { pb } from '$lib/pocketbase'
+
+	export let currentUser
 
 	let activeRoute = ''
 
@@ -12,7 +16,22 @@
 	class="sticky top-0 z-10 flex h-12 items-center justify-between border-b border-border bg-card bg-opacity-75 px-4 text-card-foreground backdrop-blur-lg backdrop-filter"
 >
 	<span class="hidden font-bold sm:inline-block">OpenAI Playground</span>
-	<div>
+	<div class="inline-flex">
+		{#if $currentUser}
+			<span class="mr-4">Logged in as {$currentUser.username}</span>
+			<form
+				method="POST"
+				action="/logout"
+				use:enhance={() => {
+					return async ({ result }) => {
+						pb.authStore.clear()
+						await applyAction(result)
+					}
+				}}
+			>
+				<button>Log out</button>
+			</form>
+		{/if}
 		<LightSwitch />
 		<ModeWatcher defaultMode={'dark'} />
 	</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
-<script>
+<script lang="ts">
 	import TopNav from '$lib/components/TopNav.svelte'
 	import { Toaster } from '$lib/components/ui/sonner'
 	import '../app.css'
+	import { currentUser } from '$lib/stores/user'
+	import type { PageData } from './$types'
+
+	export let data: PageData
+
+	// Set the current user from the data passed in from the server
+	$: currentUser.set(data.user)
 </script>
 
 <Toaster expand richColors closeButton />
-<TopNav />
+<TopNav {currentUser} />
 <div class="mx-4">
 	<slot />
 </div>
