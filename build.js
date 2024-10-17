9b1b321ec199c8ab35a2b95cd0bf074f552abab5
Corey Developer <corey.damocles@fastdms.com>
2024-10-16 17:01:33 -0700

    Update build.js

--- build.js
+++ build.js
@@ -1,10 +1,20 @@
-const fs = require('fs')
+import { readFileSync, writeFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // Path to the pdf.js file in pdf.js-extract
-const dir = 'node_modules/pdf.js-extract/lib/pdfjs/pdf.js'
+const dir = join(__dirname, 'node_modules', 'pdf.js-extract', 'lib', 'pdfjs', 'pdf.js')
 
 // Read the content of the pdf.js file
-const content = fs.readFileSync(dir, { encoding: 'utf-8' })
+const content = readFileSync(dir, { encoding: 'utf-8' })
 
 // Replace the line that causes the error
-fs.writeFileSync(dir, content.replace('"./pdf.worker.js";', `__dirname + "/pdf.worker.js";`))
+writeFileSync(
+	dir,
+	content.replace('"./pdf.worker.js";', `new URL("pdf.worker.js", import.meta.url).pathname`)
+)
+
+console.log('PDF.js file updated successfully.')
